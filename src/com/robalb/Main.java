b9830964768b229872fe14abab8365f782084a82
robalb <alberto.ventafridda@gmail.com>
2020-11-28 03:34:08 +0100

    fixed typo

--- src/com/robalb/Main.java
+++ src/com/robalb/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        system.out.println("hello world");
+        System.out.println("hello world");
     }
 }
